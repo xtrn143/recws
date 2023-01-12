d64cc7bbf8b7bf2431fde2dcd4ca802058183215
zhangyang1 <zhangyang1@chinaexpressair.com>
2023-01-12 13:34:29 +0800

    init test

--- examples/basic.go
+++ examples/basic.go
@@ -1,48 +1,45 @@
 package main
 
 import (
-	"context"
 	"github.com/recws-org/recws"
 	"log"
 	"time"
 )
 
 func main() {
-	ctx, cancel := context.WithCancel(context.Background())
+	//ctx, cancel := context.WithCancel(context.Background())
 	ws := recws.RecConn{
-		KeepAliveTimeout: 10 * time.Second,
+		//KeepAliveTimeout: 10 * time.Second,
 	}
-	ws.Dial("wss://echo.websocket.org", nil)
+	ws.Dial("ws://127.0.0.1:3563", nil)
 
-	go func() {
-		time.Sleep(2 * time.Second)
-		cancel()
-	}()
+	//go func() {
+	//	time.Sleep(2 * time.Second)
+	//	cancel()
+	//}()
 
 	for {
 		select {
-		case <-ctx.Done():
-			go ws.Close()
-			log.Printf("Websocket closed %s", ws.GetURL())
-			return
+		//case <-ctx.Done():
+		//	go ws.Close()
+		//	log.Printf("Websocket closed %s", ws.GetURL())
+		//	return
 		default:
 			if !ws.IsConnected() {
 				log.Printf("Websocket disconnected %s", ws.GetURL())
-				continue
+				<-time.After(3 * time.Second)
 			}
 
 			if err := ws.WriteMessage(1, []byte("Incoming")); err != nil {
 				log.Printf("Error: WriteMessage %s", ws.GetURL())
-				return
+				<-time.After(3 * time.Second)
 			}
 
-			_, message, err := ws.ReadMessage()
+			_, _, err := ws.ReadMessage()
 			if err != nil {
 				log.Printf("Error: ReadMessage %s", ws.GetURL())
-				return
+				<-time.After(3 * time.Second)
 			}
-
-			log.Printf("Success: %s", message)
 		}
 	}
 }
